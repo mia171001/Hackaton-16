edd98dfd7db2bf99ecfb96f418a6954df0eeed5a
mia171001 <alejandramia171001@gmail.com>
2024-05-11 20:07:45 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import AddTask from "./components/addTask";
+import './App.css';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (newTask) => {
+    setTasks([...tasks, { ...newTask, status: false }]);
+  };
+
+  // Función para eliminar una tarea
+  const deleteTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
+
+  // Función para marcar o desmarcar una tarea como completada
+  const toggleTaskStatus = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index].status = !updatedTasks[index].status;
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <div className="container">
+      <h1>TodoList</h1>
+      <AddTask className="addT" addTask={addTask} />
+      <ul id="task-list">
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <input
+              type="checkbox"
+              checked={task.status}
+              onChange={() => toggleTaskStatus(index)}
+            />
+            <span
+            >
+              {task.title}
+            </span>
+            <button className="btnD" onClick={() => deleteTask(index)}>Eliminar</button>
+          </li>
+        ))}
+
+      </ul>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/addTask.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+function AddTask({ addTask }) {
+    const [taskInput, setTaskInput] = useState("");
+
+    const handleInputChange = (event) => {
+        setTaskInput(event.target.value);
+    };
+
+    const handleAddTask = () => {
+        if (taskInput.trim() === "") {
+            alert("Por favor, ingresa una tarea válida.");
+            return;
+        }
+
+        const newTask = { title: taskInput, status: true };
+        addTask(newTask);
+        setTaskInput("");
+    };
+
+
+
+    return (
+        <form id="todo-form" >
+            <input
+                type="text"
+                placeholder="Añade una nueva tarea"
+                value={taskInput}
+                onChange={handleInputChange}
+            />
+            <button className="btnAdd" onClick={handleAddTask} type="button">
+                Añadir Tarea
+            </button>
+        </form>
+    );
+}
+
+export default AddTask;
